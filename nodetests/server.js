ab291d8e881a78b363016e018b27bd15bec70ce1
santoshkiran2710 <31537828+santoshkiran2710@users.noreply.github.com>
2018-10-08 12:07:27 +0530

    node module updates.

---
+++ nodetests/server.js
@@ -0,0 +1,64 @@
+/*
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = 4;
+
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    http.createServer(function(req, res) {
+        res.writeHead(200);
+        res.end('process ' + process.pid + ' says hello!');
+    }).listen(8000);
+}
+*/
+
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  masterProcess();
+} else {
+  childProcess();  
+}
+
+
+
+function masterProcess() {
+  console.log(`Master ${process.pid} is running`);
+	const workers = [];
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    console.log(`Forking process number ${i}...`);
+
+    const worker = cluster.fork();
+    workers.push(worker);
+
+    // Listen for messages from worker
+    worker.on('message', function(message) {
+      console.log(`Master ${process.pid} recevies message '${JSON.stringify(message)}' from worker ${worker.process.pid}`);
+    });
+  }
+
+  // Send message to the workers
+  workers.forEach(function(worker) {
+    console.log(`Master ${process.pid} sends message to worker ${worker.process.pid}...`);
+    worker.send({ msg: `Message from master ${process.pid}` });    
+  }, this);
+}
+
+function childProcess() {
+  console.log(`Worker ${process.pid} started`);
+
+  process.on('message', function(message) {
+    console.log(`Worker ${process.pid} recevies message '${JSON.stringify(message)}'`);
+  });
+
+  console.log(`Worker ${process.pid} sends message to master...`);
+  process.send({ msg: `Message from worker ${process.pid}` });
+
+  console.log(`Worker ${process.pid} finished`);
+}
